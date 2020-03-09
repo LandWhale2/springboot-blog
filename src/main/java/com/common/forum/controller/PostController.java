eb0833ca74b4a58b8bbf4ac9975731ea9b34736b
kcuyh <khj879123@gmail.com>
2020-03-09 17:19:56 +0900

    comment service

---
+++ src/main/java/com/common/forum/controller/PostController.java
@@ -0,0 +1,89 @@
+package com.common.forum.controller;
+
+import lombok.RequiredArgsConstructor;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+
+  private final PostService postService;
+
+//  @GetMapping("/{id}")
+//  public String findByPost(@PathVariable Long id, Model model) {
+//    Post post = postService.findByIdAndStatus(id, PostStatus.Y);
+//    if (post == null) {
+//      throw new NotFoundException(id + " not found");
+//    }
+//    model.addAttribute("post", post);
+//    return "post/post";
+//  }
+//
+//  @GetMapping("/new")
+//  public String newPost(PostDto postDto) {
+//    return "post/new";
+//  }
+//
+//  @GetMapping("/edit/{id}")
+//  public String editPost(@PathVariable Long id, Model model) {
+//    Post post = postService.findByIdAndStatus(id, PostStatus.Y);
+//    if (post == null) {
+//      throw new NotFoundException(id + " not found");
+//    }
+//    PostDto createPost = new PostDto();
+//    createPost.setCategoryId(post.getCategory().getId());
+//    createPost.setCategoryName(post.getCategory().getName());
+//    createPost.setTitle(post.getTitle());
+//    createPost.setCode(post.getCode());
+//    createPost.setContent(post.getContent());
+//    createPost.setId(id);
+//    model.addAttribute("editPost", createPost);
+//    return "post/edit";
+//  }
+
+  @PostMapping
+  public String createPost(@ModelAttribute @Valid PostDto createPost, BindingResult bindingResult, Model model) {
+    if (bindingResult.hasErrors()) {
+      return "post/new";
+    }
+    Post post = new Post(createPost.getTitle(),
+      createPost.getContent(),
+      createPost.getCode(),
+      PostStatus.Y,
+      new Category(createPost.getCategoryId()));
+    Post newPost = postService.createPost(post);
+    model.addAttribute("post", newPost);
+    return "redirect:/posts/" + newPost.getId();
+  }
+
+  @PostMapping("/{id}/edit")
+  public String modifyPost(@PathVariable Long id, @ModelAttribute("editPost") @Valid PostDto createPost, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return "post/edit";
+    }
+    postService.updatePost(id, new Post(
+      createPost.getTitle(),
+      createPost.getContent(),
+      createPost.getCode(),
+      PostStatus.Y,
+      new Category(createPost.getCategoryId())
+    ));
+    return "redirect:/posts/" + id;
+  }
+
+  @PostMapping("{id}/delete")
+  public String deletePost(@PathVariable Long id) {
+    postService.deletePost(id);
+    return "redirect:/#/";
+  }
+}
---
+++ src/main/java/com/common/forum/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.common.forum.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentService {
+
+  private final CommentRepository commentRepository;
+
+  public Comment createComment(Comment comment) {
+    comment.setRegDate(LocalDateTime.now());
+    return commentRepository.save(comment);
+  }
+
+  public void deleteComment(Long commentId) {
+    commentRepository.delete(commentId);
+  }
+}
