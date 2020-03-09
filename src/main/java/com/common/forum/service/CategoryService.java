929b2e61d280f9e8304720cb9d43758cbb8bdc34
kcuyh <khj879123@gmail.com>
2020-03-09 17:12:29 +0900

    category service

---
+++ src/main/java/com/common/forum/service/CategoryService.java
@@ -0,0 +1,49 @@
+package com.common.forum.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CategoryService {
+
+  private final CategoryRepository categoryRepository;
+
+  public Category createCategory(Category category) {
+    category.setRegDate(LocalDateTime.now());
+    return categoryRepository.save(category);
+  }
+
+  public void delete(Long id) {
+    categoryRepository.delete(id);
+  }
+
+  public void updateCategory(Category category) {
+    Category oldCategory = categoryRepository.findOne(category.getId());
+    if (oldCategory != null) {
+      oldCategory.setName(category.getName());
+    }
+  }
+
+  @Transactional(readOnly = true)
+  public Page<Category> findAll(Pageable pageable) {
+    return categoryRepository.findAll(pageable);
+  }
+
+  @Transactional(readOnly = true)
+  public List<Category> findAll() {
+    return categoryRepository.findAll();
+  }
+
+  public Category findOne(Long id) {
+    return categoryRepository.findOne(id);
+  }
+}
