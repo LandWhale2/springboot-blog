97a7f61f61912128cd7ae3108cf82d76649bb5a9
kcuyh <khj879123@gmail.com>
2020-03-10 16:07:11 +0900

    not found html

--- src/main/java/com/common/forum/CommentTestApplication.java
+++ src/main/java/com/common/forum/CommentTestApplication.java
@@ -1,13 +1,25 @@
 package com.common.forum;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
+
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {CommentTestApplication.class, Jsr310JpaConverters.class})
 public class CommentTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommentTestApplication.class, args);
 	}
 
+	
+	@Bean
+	public SpringDataDialect springDataDialect() {
+	  return new SpringDataDialect();
+	}
 }
